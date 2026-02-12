a66c8c358d9e9cdff855c5e0b6aed4bc213c15b3
Avinashy07 <avinashyakkali07@gmail.com>
2026-02-12 10:25:58 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to plaindrome checker ");
+        System.out.println("version : 1.0");
+        System.out.println("system instalized successfully");
+    }
+
+}
