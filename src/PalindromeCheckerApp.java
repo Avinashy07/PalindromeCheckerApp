89245c8668f3a520e426f82a6ad540ed177a31b3
johnComputer <johnComputer@gmail.com>
2026-02-19 10:28:14 +0530

    Added by array reversed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,6 +3,25 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to plaindrome checker ");
         System.out.println("version : 1.0");
         System.out.println("system instalized successfully");
+
+        String word = "madam";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string using a loop
+        for(int i=word.length()-1;i>=0;i--){
+            reversed=reversed+word.charAt(i);
+        }
+
+        // Check if original and reversed strings are equal
+        if (word.equals(reversed)) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+
+
     }
 
 }
