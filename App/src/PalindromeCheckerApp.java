7e180787a2215e87cc1fe14d07e5fd6530e6287a
Avinash07 <avinashyakkali07@gmail.com>
2026-02-26 09:19:04 +0530

    Avinash added uc7

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,49 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+    public static boolean isPalindrome(String input) {
+
+        // Convert to lowercase and remove non-alphanumeric characters
+        input = input.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (char ch : input.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        // Compare front and rear elements
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== UC7: Deque-Based Optimized Palindrome Checker =====");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean result = isPalindrome(input);
+
+        if (result) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
